d747c2b3253a181deceac08c65f3de1621fb5838
sadan0707 <sadan0707@gmail.com>
2014-10-19 16:07:21 +0200

    czesc 1

---
+++ Spis/src/Spis.java
@@ -0,0 +1,65 @@
+import java.util.*;
+public class Spis {
+
+	
+	public static void main(String[] args) {
+		Inwentor[] urzadzenie = new Inwentor [1];
+		
+		urzadzenie[0] = new Inwentor("kardiomonitor", "M8002A", "DE00000000","MP30", )
+
+	}
+
+}
+
+
+	class Inwentor
+	{
+		private String nazwa, model, numer_fabryczny, dystrybutor, serwis;
+		private Date rok_produkcji, data_uruchomienia; 
+		
+		public Inwentor (String n,  String m, 
+				String r, String sn, String p, 
+				int r_prod, int d_uruch, 
+				String dyst, String serw) 
+		{
+				
+			nazwa = n;
+			model = m;
+			numer_fabryczny = sn;
+			dystrybutor = dyst;
+			serwis = serw;
+			GregorianCalendar 
+			rok_produkcji = r_prod;
+			data_uruchomienia = d_uruch;
+			
+		}
+		
+		public String dajNazwe() {
+			return nazwa;
+		}
+		
+		public String dajModel() {
+			return model;
+			
+		}
+		
+		public String dajNumer() {
+			return numer_fabryczny ;
+		}
+		
+		public String dajDystrybutora() {
+			return dystrybutor;
+		}
+		
+		public String dajSerwis() {
+			return serwis;
+		}
+		
+		public Date dajRokProdukcji() {
+			return rok_produkcji;
+		}
+		
+		public Date dajDateUruchomienia() {
+			return data_uruchomienia;
+		}
+	}
